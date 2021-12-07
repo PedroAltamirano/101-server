83fee459b716a8740ced39de31ae12dcf33f675c
PedroAltamirano <pedroaal04@gmail.com>
2021-12-07 14:23:55 -0500

    added models + connection

---
+++ db_connection.js
@@ -0,0 +1,26 @@
+const { Sequelize, Model, DataTypes } = require('sequelize');
+require('dotenv').config()
+
+const MovieModel = require('./models/movie')
+const ActorModel = require('./models/actor')
+const GenreModel = require('./models/genre')
+console.log(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSW)
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSW, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql'
+});
+
+const Movie = MovieModel(sequelize, Sequelize)
+const Actor = ActorModel(sequelize, Sequelize)
+const Genre = GenreModel(sequelize, Sequelize)
+
+sequelize
+  .sync({ force: false })
+  .then(() => console.log('tablas sync'))
+
+module.exports = {
+  Movie,
+  Actor,
+  Genre
+}

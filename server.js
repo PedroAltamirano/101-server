e097154579f02bbffbd37fb1c9c98da06b8d2055
PedroAltamirano <pedroaal04@gmail.com>
2021-12-08 00:53:38 -0500

    test vercel

--- server.js
+++ server.js
@@ -1,38 +1,37 @@
 const express = require('express')
 const cors = require('cors')
-require('./db_connection')
-const multer = require('multer');
-const storage = multer.diskStorage({
-  destination: 'public/uploads',
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname)
-  }
-})
-const upload = multer({
-  storage,
-  limits: { fileSize: 1000000 }
-});
+// require('./db_connection')
+// const multer = require('multer');
+// const storage = multer.diskStorage({
+//   destination: 'public/uploads',
+//   filename: (req, file, cb) => {
+//     cb(null, Date.now() + '-' + file.originalname)
+//   }
+// })
+// const upload = multer({
+//   storage,
+//   limits: { fileSize: 1000000 }
+// });
 
 const app = express()
 const port = 3000
 
-const apiRouter = require('./router/api')
-
 app.use(cors())
 
 // json, and x-www-form-urlencoded data
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+// app.use(express.json())
+// app.use(express.urlencoded({ extended: true }))
 
 // multipart/form-data
-app.use(upload.single('image'))
-app.use(express.static('public'))
-
-app.use('/api', apiRouter)
+// app.use(upload.single('image'))
+// app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.send('101 Server online')
 })
+
+// const apiRouter = require('./router/api')
+// app.use('/api', apiRouter)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)

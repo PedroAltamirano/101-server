35bc374c9f6e2e4ac199067d0d7860b2bfcee691
PedroAltamirano <pedroaal04@gmail.com>
2021-12-07 21:12:57 -0500

    body-parser to express

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express = require('express')
 const cors = require('cors')
-const bodyParser = require('body-parser')
 require('./db_connection')
+const multer = require('multer');
+const upload = multer();
 
 const app = express()
 const port = 3000
 
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-
 const apiRouter = require('./router/api')
+
+app.use(cors())
+
+// json, and x-www-form-urlencoded data
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// multipart/form-data
+app.use(upload.array())
+app.use(express.static('public'))
+
 app.use('/api', apiRouter)
 
 app.listen(port, () => {
